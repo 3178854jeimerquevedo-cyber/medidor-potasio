48196568cea1d71debdba58788cab4cf12ed7864
3178854jeimerquevedo-cyber <3178854.jeimerquevedo@gmail.com>
2026-09-05 14:41:13 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+function nuevaMedicion() {
+
+    // Generamos una lectura de prueba
+    // para simular el sensor TCS230.
+    let lectura = Math.floor(Math.random() * 500) + 300;
+
+    // Valores simulados del sensor
+    let rojo = lectura;
+
+    let verde = Math.floor(lectura * 0.80);
+
+    let azul = Math.floor(lectura * 0.70);
+
+    // Conversión PROVISIONAL.
+    // Estos valores NO son mediciones reales
+    // de potasio.
+    let potasio = Math.round((900 - lectura) / 8);
+
+    // Evitamos valores negativos
+    if (potasio < 0) {
+        potasio = 0;
+    }
+
+    // Determinamos el nivel
+    let nivelTexto;
+
+    if (potasio < 40) {
+
+        nivelTexto = "NIVEL: BAJO";
+
+    } else if (potasio <= 120) {
+
+        nivelTexto = "NIVEL: ADECUADO";
+
+    } else {
+
+        nivelTexto = "NIVEL: ALTO";
+    }
+
+    // Mostramos los resultados
+    document.getElementById("potasio").textContent = potasio;
+
+    document.getElementById("lectura").textContent = lectura;
+
+    document.getElementById("rojo").textContent = rojo;
+
+    document.getElementById("verde").textContent = verde;
+
+    document.getElementById("azul").textContent = azul;
+
+    document.getElementById("nivel").textContent = nivelTexto;
+}
